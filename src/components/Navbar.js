834c5235a9c58a6a3193eda2ab5f80e725c8403b
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-27 09:24:00 -0500

    Add navbar data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
+import navbarItems from '../data';
 
 const Navbar = () => {
   const [open, setOpen] = useState(true);
 
   return (
     <>
-      <nav className="flex">
+      <div className="flex max-h-[90vh]">
         <div
           className={`${
             open ? 'w-72' : 'w-20'
-          } h-screen bg-secondary relative duration-300 `}
+          } h-screen  relative duration-300 `}
         >
           <FaAngleDoubleRight
             className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-secondary ${
@@ -18,11 +19,31 @@ const Navbar = () => {
             }`}
             onClick={() => setOpen(!open)}
           />
+          <div
+            className={`${
+              open ? 'w-36' : 'w-20'
+            } cursor-pointer duration-300 my-4 mx-auto border-rounded`}
+          >
+            <p>Logo</p>
+          </div>
+          <div className="flex flex-col items-center">
+            {navbarItems.map((item) => (
+              <div
+                key={item.name}
+                className={`${
+                  open ? 'w-36' : 'w-20'
+                } h-10  flex items-center  cursor-pointer duration-300 hover:bg-primary`}
+              >
+                {item.name}
+              </div>
+            ))}
+          </div>
+
         </div>
         <div className="p-7 text-2xl font-semibold flex-1 h-screen">
           <h1>Homepage</h1>
         </div>
-      </nav>
+      </div>
     </>
   );
 };
