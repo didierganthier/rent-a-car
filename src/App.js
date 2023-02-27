164a6b7a476782775f52cb7ced36954274fb7106
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-27 08:42:02 -0500

    Create navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Navbar from './components/Navbar';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <LoginScreen />
       <RegisterScreen />
     </div>
