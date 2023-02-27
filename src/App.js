ad7315fbed27ff864010a8ffb7d589ea9a61abfb
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-27 02:05:37 -0500

    Add login redux functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-export default function App() {
+import LoginScreen from './screens/LoginScreen';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">
+      <LoginScreen />
+    </div>
   );
 }
+
+export default App;
