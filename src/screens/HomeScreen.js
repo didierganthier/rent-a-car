cc754287b2b25d6c67b5d4d44ad07042ee7893a8
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-03-09 18:33:35 -0500

    Add delete option

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom';
 import Loader from '../components/Loader';
 import { getCars } from '../redux/actions/CarAction';
 import { BsFillArrowLeftCircleFill } from 'react-icons/bs';
-
+import { deleteCar } from '../redux/actions/CarAction';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -59,72 +59,85 @@ const HomeScreen = () => {
             "
         />
       </div>
-      {cars && (
+      {cars.length >0 ? (
         <Swiper
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={'auto'}
-          initialSlide={1}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          
-          modules={[EffectCoverflow, Pagination]}
-          className="mySwiper max-w-[400px] md:max-w-[1000px] lg:max-w-full overflow-hidden relative" style={{ margin: '0 auto' }}
-          ref={swiperRef}
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        initialSlide={1}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper max-w-[400px] md:max-w-[1000px] lg:max-w-full overflow-hidden relative" style={{ margin: '0 auto' }}
+        ref={swiperRef}
+      >
+        {cars.map((car) => (
+          <SwiperSlide
+            key={car.id}
+            className="max-w-[300px] mr-[60px] ml-[1.2rem] shadow-md rounded-2xl overflow-hidden px-4 py-2"
+          >
+            <div>
+              <img
+                src={car.image}
+                className="w-full max-h-[300px] rounded-full h-96 object-cover"
+                alt="car"
+              />
+
+              <div className="card-body mb-[4rem]">
+                <h3
+                  className="font-bold
+                                      text-xl text-gray-900 px-6 mt-[1rem] mb-[1rem]
+                                  "
+                >
+                  {car.name}
+                </h3>
+                <p className="card-text">{truncate(car.description, 100)}</p>
+                <button className="bg-[#da2525] text-white px-4 py-2 rounded-full mt-4" onClick={() => handleDelete(car.id)}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+
+        <div
+          className="absolute text-[1.8rem]
+                          top-1/3 left-0 shadow-md z-10 text-[#256BDA]
+                          rounded-full p-3 cursor-pointer hover:animate-bounce
+                          hidden md:block
+                          "
+          onClick={handlePrev}
         >
-          {cars.map((car) => (
-            <SwiperSlide
-              key={car.id}
-              className="max-w-[300px] mr-[60px] ml-[1.2rem] shadow-md rounded-2xl overflow-hidden px-4 py-2"
-            >
-              <NavLink className="car" to={`/car/${car.id}`}>
-                <img
-                  src={car.image}
-                  className="w-full max-h-[300px] rounded-full h-96 object-cover"
-                  alt="car"
-                />
-
-                <div className="card-body mb-[4rem]">
-                  <h3
-                    className="font-bold
-                                        text-xl text-gray-900 px-6 mt-[1rem] mb-[1rem]
-                                    "
-                  >
-                    {car.name}
-                  </h3>
-                  <p className="card-text">{truncate(car.description, 100)}</p>
-                </div>
-              </NavLink>
-            </SwiperSlide>
-          ))}
-
-          <div
-            className="absolute text-[1.8rem]
-                            top-1/3 left-0 shadow-md z-10 text-[#256BDA]
-                            rounded-full p-3 cursor-pointer hover:animate-bounce
-                            hidden md:block
-                            "
-            onClick={handlePrev}
-          >
-            <BsFillArrowLeftCircleFill />
-          </div>
-          <div
-            className="absolute text-[1.8rem]
-                            top-1/3 right-0 shadow-md z-10 rotate-180 text-[#256BDA]
-                            rounded-full p-3 cursor-pointer hover:animate-bounce
-                            hidden md:block
-                            "
-            onClick={handleNext}
-          >
-            <BsFillArrowLeftCircleFill />
-          </div>
-        </Swiper>
+          <BsFillArrowLeftCircleFill />
+        </div>
+        <div
+          className="absolute text-[1.8rem]
+                          top-1/3 right-0 shadow-md z-10 rotate-180 text-[#256BDA]
+                          rounded-full p-3 cursor-pointer hover:animate-bounce
+                          hidden md:block
+                          "
+          onClick={handleNext}
+        >
+          <BsFillArrowLeftCircleFill />
+        </div>
+      </Swiper>
+      ) : (
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold text-gray-900 mb-4">No Cars</h1>
+          <img src="https://cdn-icons-png.flaticon.com/512/4955/4955261.png" alt="undraw-Empty-re-9gll" className='w-36 h-36 my-10' />
+          <NavLink to="/addcar">
+            <button className="bg-[#da2525] text-white px-4 py-2 rounded-full mt-4">
+              Add Car
+            </button>
+          </NavLink>
+        </div>
       )}
     </>
   );
